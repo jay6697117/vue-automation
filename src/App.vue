ddcfe844aa2910a1b2330eb91a8b8d06d29dd3b4
steve zhang <871506263@qq.com>
2020-12-07 00:05:48 +0800

    提交代码于:2020-12-07-00:05:48

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 <template>
-    <div id="app">
-        <RouterView v-if="isRouterAlive" />
-    </div>
+  <div id="app">
+    <RouterView v-if="isRouterAlive" />
+  </div>
 </template>
 
 <script>
 export default {
-    provide() {
-        return {
-            reload: this.reload
-        }
+  provide() {
+    return {
+      reload: this.reload,
+    };
+  },
+  data() {
+    return {
+      isRouterAlive: true,
+    };
+  },
+  watch: {
+    $route: "routeChange",
+  },
+  methods: {
+    reload() {
+      this.isRouterAlive = false;
+      this.$nextTick(() => (this.isRouterAlive = true));
     },
-    data() {
-        return {
-            isRouterAlive: true
-        }
+    routeChange(newVal, oldVal) {
+      if (newVal.name == oldVal.name) {
+        this.reload();
+      }
     },
-    watch: {
-        $route: 'routeChange'
+  },
+  metaInfo: {
+    titleTemplate: (title) => {
+      return title
+        ? `${title} - ${process.env.VUE_APP_TITLE}`
+        : process.env.VUE_APP_TITLE;
     },
-    methods: {
-        reload() {
-            this.isRouterAlive = false
-            this.$nextTick(() => (this.isRouterAlive = true))
-        },
-        routeChange(newVal, oldVal) {
-            if (newVal.name == oldVal.name) {
-                this.reload()
-            }
-        }
-    },
-    metaInfo: {
-        titleTemplate: title => {
-            return title
-                ? `${title} - ${process.env.VUE_APP_TITLE}`
-                : process.env.VUE_APP_TITLE
-        }
-    }
-}
+  },
+};
 </script>
